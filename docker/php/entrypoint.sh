1b86031c639abc793f3a84f6c932602a4cb4eae5
Belisario Peró <belisario.pero@kwargentina.com>
2026-06-07 20:19:52 -0300

    Install Composer dependencies when vendor volume is empty

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -10,7 +10,7 @@ if [ ! -f .env ] && [ -f .env.example ]; then
     cp .env.example .env
 fi
 
-if [ ! -d vendor ]; then
+if [ ! -f vendor/autoload.php ]; then
     composer install --no-interaction --prefer-dist
 fi
 
